b20eeb55b8cee6ddd388fb2d5c0f1b5d9d1861f9
Ismail <i180516@nu.edu.pk>
2022-05-01 17:06:50 +0500

    Admin edit product screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
 import ProductListScreen from './screens/ProductListScreen'
+import ProductEditScreen from './screens/ProductEditScreen'
 
 const App = () => {
   return (
@@ -27,6 +28,10 @@ const App = () => {
             <Route path='/admin/userlist' element={<UserListScreen />} />
             <Route path='/admin/user/:id/Edit' element={<UserEditScreen />} />
             <Route path='/admin/productlist' element={<ProductListScreen />} />
+            <Route
+              path='/admin/product/:id/Edit'
+              element={<ProductEditScreen />}
+            />
             <Route path='/login' element={<LoginScreen />} />
             <Route path='/register' element={<RegisterScreen />} />
             <Route path='/profile' element={<ProfileScreen />} />
