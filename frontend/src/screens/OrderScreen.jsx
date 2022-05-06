9c4c6cc0f93fadb83f8b750f48fd27e6805844b0
Ismail <i180516@nu.edu.pk>
2022-05-06 16:07:05 +0500

    Fixed order ID issue on OrderScreen

--- frontend/src/screens/OrderScreen.jsx
+++ frontend/src/screens/OrderScreen.jsx
@@ -67,7 +67,7 @@ function OrderScreen() {
       document.body.appendChild(script)
     }
 
-    if (!order || successPay || successDeliver) {
+    if (!order || successPay || successDeliver || order._id !== orderId) {
       dispatch({ type: ORDER_PAY_RESET })
       dispatch({ type: ORDER_DELIVER_RESET })
       dispatch(getOrderDetails(orderId))
